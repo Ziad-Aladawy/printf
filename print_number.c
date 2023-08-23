d506eec482d8b78aa959fa2f39fc1d06a9a61016
Ziad-Aladawy <ziad.aladawy.se@outlook.com>
2023-08-23 04:00:22 +0300

    print_number - ziad

---
+++ print_number.c
@@ -0,0 +1,131 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if character is digit
+ * @c: the character to check
+ *
+ * Return: 1 if digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ *
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+	int y = 0;
+
+	while (*s++)
+		y++;
+	return (y);
+}
+
+/**
+ * print_number - prints a number with options
+ * @s: the base number as a string
+ * @params: the parameter struct
+ *
+ * Return: chars printed
+ */
+int print_number(char *s, params_t *params)
+{
+	unsigned int i = _strlen(s);
+	int neg = (!params->unsign && *s == '-');
+
+	if (!params->precision && *s == '0' && !s[1])
+		s = "";
+	if (neg)
+	{
+		s++;
+		i--;
+	}
+	if (params->precision != UINT_MAX)
+		while (i++ < params->precision)
+			*--s = '0';
+	if (neg)
+		*--s = '-';
+
+	if (!params->minus_flag)
+		return (print_number_right_shift(s, params));
+	else
+		return (print_number_left_shift(s, params));
+}
+
+/**
+ * print_number_right_shift - prints a number with options
+ * @s: the base number as a string
+ * @params: the parameter struct
+ *
+ * Return: chars printed
+ */
+int print_number_right_shift(char *s, params_t *params)
+{
+	unsigned int x = 0, neg, neg2, y = _strlen(s);
+	char pad_char = ' ';
+
+	if (params->zero_flag && !params->minus_flag)
+		pad_char = '0';
+	neg = neg2 = (!params->unsign && *s == '-');
+	if (neg && x < params->width && pad_char == '0' && !params->minus_flag)
+		s++;
+	else
+		neg = 0;
+	if ((params->plus_flag && !neg2) ||
+		(!params->plus_flag && params->space_flag && !neg2))
+		y++;
+	if (neg && pad_char == '0')
+		x += _putchar('-');
+	if (params->plus_flag && !neg2 && pad_char == '0' && !params->unsign)
+		x += _putchar('+');
+	else if (!params->plus_flag && params->space_flag && !neg2 &&
+		!params->unsign && params->zero_flag)
+		x += _putchar(' ');
+	while (y++ < params->width)
+		x += _putchar(pad_char);
+	if (neg && pad_char == ' ')
+		x += _putchar('-');
+	if (params->plus_flag && !neg2 && pad_char == ' ' && !params->unsign)
+		x += _putchar('+');
+	else if (!params->plus_flag && params->space_flag && !neg2 &&
+		!params->unsign && !params->zero_flag)
+		x += _putchar(' ');
+	x += _puts(s);
+	return (x);
+}
+
+/**
+ * print_number_left_shift - prints a number with options
+ * @s: the base number as a string
+ * @params: the parameter struct
+ *
+ * Return: chars printed
+ */
+int print_number_left_shift(char *s, params_t *params)
+{
+	unsigned int x = 0, neg, neg2, y = _strlen(s);
+	char pad_char = ' ';
+
+	if (params->zero_flag && !params->minus_flag)
+		pad_char = '0';
+	neg = neg2 = (!params->unsign && *s == '-');
+	if (neg && x < params->width && pad_char == '0' && !params->minus_flag)
+		s++;
+	else
+		neg = 0;
+
+	if (params->plus_flag && !neg2 && !params->unsign)
+		x += _putchar('+'), y++;
+	else if (params->space_flag && !neg2 && !params->unsign)
+		x += _putchar(' '), y++;
+	x += _puts(s);
+	while (y++ < params->width)
+		x += _putchar(pad_char);
+	return (x);
+}
+
